ea6ae7fd4c0644cc0ee50629898bb8dd4bd87727
SetNameKarin <karin.manafova@gmail.com>
2019-11-13 17:52:14 +0200

    Needs changes

---
+++ src/test/java/ru/stqa/selenium/util/DataProviders.java
@@ -0,0 +1,119 @@
+package ru.stqa.selenium.util;
+
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class DataProviders {
+    @DataProvider
+    public static Iterator<Object[]> dataProviderFirst() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/DSingleFilterByHolidays.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+    @DataProvider
+    public static Iterator<Object[]> filterByKosher() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/DSingleFilterFoodByKosher.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+    @DataProvider
+    public static Iterator<Object[]> loginPositive() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/DLoginPositive.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+    @DataProvider
+    public static Iterator<Object[]> loginNegative() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/DLoginNegativeTest.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+    @DataProvider
+    public static Iterator<Object[]> lastNameChanging() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/DLastNameOfFamilyChanging.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+
+    @DataProvider
+    public static Iterator<Object[]> dataProviderSecond() {
+        List<Object[]> data = new ArrayList();
+        data.add(new Object[]{"login1", "pssword1"});
+        data.add(new Object[]{"login2", "password2"});
+
+        return data.iterator();
+    }
+
+
+    @DataProvider
+    public Iterator<Object[]> dataProviderThird() {
+        List<Object[]> data = new ArrayList();
+
+        for(int i = 0; i < 5; ++i) {
+            data.add(new Object[]{this.generateRandomName(), this.generateRandomPassword()});
+        }
+
+        return data.iterator();
+    }
+
+    private Object generateRandomPassword() {
+        return "pass" + (new Random()).nextInt();
+    }
+
+    private Object generateRandomName() {
+        return "demo" + (new Random()).nextInt()+"@gmail.com";
+    }
+
+
+}
+
